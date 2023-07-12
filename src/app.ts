3efe2def34c327ff862617c9710cc9efbac67ee4
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-07-12 00:49:51 -0300

    Integração com Prisma ORM - Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Lucas Araujo",
+    email: "lucas@gmail.com",
+  },
+});
